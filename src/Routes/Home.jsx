59e797ae7de77df4c25deb85863cf20c08fe7ce6
Quintanilha09 <gabriel.hemendinger@gmail.com>
2024-05-12 17:42:14 -0300

    Finalizando menu e footer

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import Menu from '../Components/Menu'
+import Footer from '../Components/Footer'
+
 const Home = () => {
   return (
     <div>
       <Menu></Menu>
+      <Footer></Footer>
     </div>
   )
 }
--- src/Routes/Pontuacao.jsx
+++ src/Routes/Pontuacao.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import Menu from '../Components/Menu'
+import Footer from '../Components/Footer'
+
 const Pontuacao = () => {
   return (
     <div>
       <Menu></Menu>
+      <Footer></Footer>
     </div>
   )
 }
