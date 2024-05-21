432bdf64cd32d1b0fa87f862e51928d8ecdc5ab5
EA - Gabriel Quintanilha Cabral De Oliveira <gabriel_quintanilha@educadventista.org>
2024-05-20 22:05:58 -0300

    refinando conteúdo principal

--- src/route/Home.jsx
+++ src/route/Home.jsx
@@ -9,34 +9,29 @@ const Home = () => {
     <div>
       <Menu></Menu>
       <main>
+      <img src={LogoFalcaoAzul} alt="Logo Falcão Azul" class="logoClubeMain"/>
         <section>
-          <h2>Quem somos</h2>
-          <p>O Clube de Desbravadores da Igreja Adventista ensina habilidades práticas e valores,<br /> como disciplina e civismo, através de atividades. 
-            <br />Com acampamentos anuais que incluem ensinamentos de sobrevivência e momentos<br /> espirituais, o objetivo dos Desbravadores é orientar os jovens no caminho espiritual
-            <br /> e promover sua saúde mental, espiritual e física. O Clube Falcão Azul tem mais de<br /> 43 anos de experiência.
+          <h2 class="h2quemSomos">Quem somos</h2>
+          <p >O Clube de Desbravadores da Igreja Adventista ensina habilidades práticas e valores,<br /> como disciplina e civismo, através de atividades. 
+            Com acampamentos anuais que <br />incluem ensinamentos de sobrevivência e momentos espirituais, o objetivo dos <br />Desbravadores é orientar os jovens no caminho espiritual
+            e promover sua saúde mental,<br /> espiritual e física. O Clube Falcão Azul tem mais de 43 anos de experiência.
           </p>
         </section>
 
         <section>
-          <div>
-            <h2>Até que idade posso participar?</h2>
-            <p>Dos 10 aos 16 anos de idade.</p>
-          </div>
+          <h2 class="h2IdadePart">Até que idade posso participar?</h2>
+          <p>Dos 10 aos 16 anos de idade.</p>
         </section>
 
         <section>
-          <div>
-            <h2>Horário e Local das Reuniões</h2>
-            <p>Sábado das 14:45h às 17:00h 
-              Local: Rua Piratini 163, Jardim Branca Flor, CEP: 06855-620 - Igreja Adventista <br />
+          <h2 class="h2HorarioLocal">Horário e Local das Reuniões</h2>
+          <p>Sábado das 14:45h às 17:00h 
+            Local: Rua Piratini 163, Jardim Branca Flor, CEP: 06855-620 - Igreja Adventista <br />
 
-              Domingo das 08:45h às 12:00h 
-              Local: Rua Cerro Largo, Jardim Branca Flor, CEP: 06855-740 - Rotatória da praça
-            </p>
-          </div>
+            Domingo das 08:45h às 12:00h 
+            Local: Rua Cerro Largo, Jardim Branca Flor, CEP: 06855-740 - Rotatória da praça
+          </p>
         </section>
-
-        <img src={LogoFalcaoAzul} alt="Logo Falcão Azul" class="logoClubeMain"/>
       </main>
       <Footer></Footer>
     </div>
