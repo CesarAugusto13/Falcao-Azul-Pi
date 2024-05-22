ad10390c8718ec9aa07394081f198736b2605d06
Gabriel Quintanilha <gabriel.hemendinger@gmail.com>
2024-05-21 22:23:39 -0300

    arrumando scroller e fonts

--- src/route/Home.jsx
+++ src/route/Home.jsx
@@ -1,41 +1,46 @@
-import React from 'react'
-import Menu from '../Components/Menu'
-import Footer from '../Components/Footer'
-import '../assets/public/Home.css'
-import LogoFalcaoAzul from '../assets/images/logo.png'
+import React from 'react';
+import Menu from '../Components/Menu';
+import Footer from '../Components/Footer';
+import '../assets/public/Home.css';
+import LogoFalcaoAzul from '../assets/images/logo.png';
+
+import { motion } from 'framer-motion'
 
 const Home = () => {
   return (
     <div>
-      <Menu></Menu>
-      <main>
-      <img src={LogoFalcaoAzul} alt="Logo Falcão Azul" class="logoClubeMain"/>
+      <Menu />
+      <main className="main"> 
+        <img src={LogoFalcaoAzul} alt="Logo Falcão Azul" className="logoClubeMain" />
         <section>
-          <h2 class="h2quemSomos">Quem somos</h2>
-          <p >O Clube de Desbravadores da Igreja Adventista ensina habilidades práticas e valores,<br /> como disciplina e civismo, através de atividades. 
-            Com acampamentos anuais que <br />incluem ensinamentos de sobrevivência e momentos espirituais, o objetivo dos <br />Desbravadores é orientar os jovens no caminho espiritual
-            e promover sua saúde mental,<br /> espiritual e física. O Clube Falcão Azul tem mais de 43 anos de experiência.
+          <h2 className="josefin-sans-heading">Quem somos</h2>
+          <p className="open-sans-body">
+            O Clube de Desbravadores da Igreja Adventista ensina habilidades práticas e valores,
+            como disciplina e civismo, através de atividades. Com acampamentos anuais que 
+            incluem ensinamentos de sobrevivência e momentos espirituais, o objetivo dos 
+            Desbravadores é orientar os jovens no caminho espiritual e promover sua saúde mental,
+            espiritual e física. O Clube Falcão Azul tem mais de 43 anos de experiência.
           </p>
         </section>
 
         <section>
-          <h2 class="h2IdadePart">Até que idade posso participar?</h2>
-          <p>Dos 10 aos 16 anos de idade.</p>
+          <h2 className="josefin-sans-heading">Até que idade posso participar?</h2>
+          <p className="open-sans-body">Dos 10 aos 16 anos de idade.</p>
         </section>
 
         <section>
-          <h2 class="h2HorarioLocal">Horário e Local das Reuniões</h2>
-          <p>Sábado das 14:45h às 17:00h 
+          <h2 className="josefin-sans-heading">Horário e Local das Reuniões</h2>
+          <p className="open-sans-body">
+            Sábado das 14:45h às 17:00h 
             Local: Rua Piratini 163, Jardim Branca Flor, CEP: 06855-620 - Igreja Adventista <br />
-
             Domingo das 08:45h às 12:00h 
             Local: Rua Cerro Largo, Jardim Branca Flor, CEP: 06855-740 - Rotatória da praça
           </p>
         </section>
       </main>
-      <Footer></Footer>
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
